3482b0946ca73579a13e29f00f064223e324c7c3
Samu <151155051+Samucahub@users.noreply.github.com>
2025-10-04 17:34:10 +0100

    Convert Produto to data class and simplify methods
    Refactor Produto class to data class with default quantity

--- Kotlin/Produtos.kt
+++ Kotlin/Produtos.kt
@@ -1,31 +1,8 @@
 package com.exemplo.natabase
 
-class Produto(
-    private val nome: String,
-    private val tipo: String,
-    quantidade: Int
-) {
-    private var quantidade: Int = if (quantidade >= 0) quantidade else 0
+data class Produto(
+    val nome: String,
+    val tipo: String,
+    var quantidade: Int = 0
+)
 
-    fun getNome(): String = nome
-    fun getTipo(): String = tipo
-    fun getQuantidade(): Int = quantidade
-
-    fun aumentarQuantidade(valor: Int) {
-        val v = if (valor > 0) valor else 0
-        quantidade += v
-    }
-
-    fun diminuirQuantidade(valor: Int) {
-        val v = if (valor > 0) valor else 0
-        quantidade = (quantidade - v).coerceAtLeast(0)
-    }
-
-    fun exibirDetalhes(): String {
-        return "Nome: $nome\nTipo: $tipo\nQuantidade: $quantidade\n"
-    }
-
-    fun zerar() {
-        quantidade = 0
-    }
-}
